3b81c38aee580a45b11dee43468da8c46be269a1
Rajesh <itsyaboiinitial1ze@gmail.com>
2023-08-30 14:53:59 +0530

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  stripe: {},
+  api: 'http://127.0.0.1:3000/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  stripe: {},
+  api: 'http://127.0.0.1:3000/',
+};
